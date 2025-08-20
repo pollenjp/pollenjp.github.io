5f7551f41b0b79fdbfe233b0573e0bd480ed162c
pollenjp <polleninjp@gmail.com>
2025-08-20 11:38:03 +0900

    Add thumbnail for career-timeline

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,16 @@ function App() {
         <img src={imgUrl} alt="pollenjp" style={{ width: '100px', height: '100px' }} />
       </div>
       <br />
-      <ul style={{ listStyle: 'none', textAlign: 'left', padding: 0 }}>
+      <ul style={{ textAlign: 'left', padding: 0 }}>
         <li>
           <a href="https://scrapbox.io/pollenJP-Portfolio/README">Portfolio</a>
         </li>
         <li>
           <a href="https://pollenjp.github.io/career-timeline">Career Timeline</a>
+          <br />
+          <a href="https://pollenjp.github.io/career-timeline">
+            <img src="https://i.gyazo.com/4fd6c476aafef08fba9b7a3cdfa148b3.png" alt="career-timeline" style={{ height: '15em' }} />
+          </a>
         </li>
       </ul>
       <h2 style={{ textAlign: 'left' }}>経歴</h2>
